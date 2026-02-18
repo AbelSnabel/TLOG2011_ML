4235cb791994f2c9dfebf5747ff01dc311a19552
AbelSnabel <emilapa@stud.ntnu.no>
2026-02-18 14:44:28 +0100

    calculates lateness for each row + fixes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-def getdata(path):
+def get_preprocess():
+    """
+    removes zero-values and splits data into train_data,test_data
+    """
+    path = r'data\all_waybill_info_meituan_0322.csv'
+    df = pd.read_csv(path, index_col=0)
 
-    df = pd.read_csv(r'data\all_waybill_info_meituan_0322.csv', index_col=0)
-    train_data = df[0:int(len(df)*0.80)]
-    test_data = df[int(len(df)*0.80):]
-
-    print(df.shape)
 
     df = df[df['is_courier_grabbed'] != 0]
     df = df[df['arrive_time'] != 0]
@@ -17,4 +17,19 @@ def getdata(path):
     df = df[df['grab_lat'] != 0]
     df = df[df['grab_lng'] != 0]
 
-    return df
+    tolerance = 10    
+    late_delivery = []
+    for index, row in df.iterrows():
+        row_lateness = [1 if row["arrive_time"] > (row["estimate_arrived_time"] + tolerance) else 0]
+        late_delivery.append(row_lateness)
+    late_delivery = pd.DataFrame(late_delivery)
+    df = pd.concat([df,late_delivery],axis=1)
+
+
+    train_data = df[0:int(len(df)*0.80)]
+    test_data = df[int(len(df)*0.80):]
+
+    return train_data, test_data, df
+
+
+get_preprocess()
